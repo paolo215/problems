2cbe83ecb81208a3997aaef920ee8d7fc7604a46
paolo215 <paolo_villanueva215@yahoo.com>
2017-06-09 23:21:04 -0400

    Updated unit tests

--- TechieDelight/test_array.py
+++ TechieDelight/test_array.py
@@ -17,30 +17,26 @@ def sameArray(output, expected):
 
 
 class UtilityTest(unittest.TestCase):
-    def test_utility_sameArray1(self):
+    def test_utility_sameArray(self):
         a = [(1,2)]
         b = [(1,2)]
         self.assertTrue(sameArray(a, b))
 
-    def test_utility_sameArray2(self):
         a = []
         b = [(1,2)]
         self.assertTrue(not sameArray(a, b))
 
     
-    def test_utility_sameArray3(self):
         a = [(3,5), (1,2)]
         b = [(1,2), (3,5)]
         self.assertTrue(sameArray(a, b))
 
 
-    def test_utility_sameArray4(self):
         a = [(3,5), (1,2)]
         b = [(1,2), (3,5), (5,5)]
         self.assertTrue(not sameArray(a, b))
 
 
-    def test_utility_sameArray5(self):
         a = [(3,5), (1,2)]
         b = [(1,2), (5,5)]
         self.assertTrue(not sameArray(a, b))
@@ -49,41 +45,33 @@ class UtilityTest(unittest.TestCase):
 class ArrayTest(unittest.TestCase):
 
 
-    def test_sumPairs1(self):
+    def test_sumPairs(self):
         input_array = [8, 7, 2, 5, 3, 1]
         total = 10
         pairs = ar.sumPairs(input_array, total)
         expectedPairs = [(8,2), (7,3)]
-       
         self.assertTrue(sameArray(pairs, expectedPairs))
       
 
-    def test_sumPairs2(self):
         input_array = [1, 2, 3, 4, 5]
         total = 10
-
         pairs = ar.sumPairs(input_array, total)
         expectedPairs = []
-
         self.assertTrue(sameArray(pairs, expectedPairs))
 
 
-    def test_sumPairs3(self):
         input_array = [0]
         total = 10
-
         pairs = ar.sumPairs(input_array, total)
         expectedPairs = [(1,1)]
-
         self.assertTrue(not sameArray(pairs, expectedPairs))
 
-    def test_zero_sub_array1(self):
+    def test_zero_sub_array(self):
         input_array = [4, 2, -3, -1, 0, 4]
         pairs = ar.zero_sub_array(input_array)
         expectedPairs = [(-3,-1,0,4), (0,)]
         self.assertTrue(sameArray(pairs, expectedPairs))
     
-    def test_zero_sub_array2(self):
         input_array = [3, 4, -7, 3, 1, 3, 1, -4, -2, -2]
         pairs = ar.zero_sub_array(input_array)
         expectedPairs = [(3,4,-7), (4,-7,3), (3,1,-4), (3,1,3,1,-4,-2,-2), \
@@ -91,14 +79,13 @@ class ArrayTest(unittest.TestCase):
         self.assertTrue(sameArray(pairs, expectedPairs))
 
 
-    def test_zero_sub_array3(self):
         input_array = [10, 10]
         pairs = ar.zero_sub_array(input_array)
         expectedPairs = []
         self.assertTrue(pairs == expectedPairs)
 
 
-    def test_binary_array_sort1(self):
+    def test_binary_array_sort(self):
         input_array = [1,0,1,0,1,0,0,1]
         output = ar.binary_array_sort(input_array)
         expectedOutput = [0,0,0,0,1,1,1,1]
@@ -106,7 +93,6 @@ class ArrayTest(unittest.TestCase):
         self.assertTrue(output == expectedOutput)
 
 
-    def test_binary_array_sort2(self):
         input_array = [1, 0]
         output = ar.binary_array_sort(input_array)
         expectedOutput = [0, 1]
@@ -114,14 +100,13 @@ class ArrayTest(unittest.TestCase):
         self.assertTrue(output == expectedOutput)
 
 
-    def test_binary_array_sort3(self):
         input_array = [1]
         output = ar.binary_array_sort(input_array)
         expectedOutput = [0]
 
         self.assertTrue(not (output == expectedOutput))
 
-    def test_find_duplicate1(self):
+    def test_find_duplicate(self):
         arr = [1, 2, 3, 4, 4]
         output = ar.find_duplicate(arr)
         expected = 4
@@ -129,7 +114,6 @@ class ArrayTest(unittest.TestCase):
         self.assertTrue(output == expected)
 
 
-    def test_find_duplicate2(self):
         arr = [1, 2, 3, 4, 2]
         output = ar.find_duplicate(arr)
         expected = 2
@@ -137,7 +121,6 @@ class ArrayTest(unittest.TestCase):
         self.assertTrue(output == expected)
 
 
-    def test_find_duplicate3(self):
         arr = [1, 2, 3, 4]
         output = ar.find_duplicate(arr)
         expected = None
@@ -145,21 +128,18 @@ class ArrayTest(unittest.TestCase):
         self.assertTrue(output == expected)
 
 
-    def test_largest_sub_array_formed_by_consecutive_integers1(self):
+    def test_largest_sub_array_formed_by_consecutive_integers(self):
         arr = [2,0,2,1,4,3,1,0]
         output = ar.largest_sub_array_formed_by_consecutive_integers(arr)
         expected = [0,2,1,4,3]
          
         self.assertTrue(output == expected)
 
-    def test_largest_sub_array_formed_by_consecutive_integers2(self):
         arr = [1,2,3,0,5,4,3,2,1]
         output = ar.largest_sub_array_formed_by_consecutive_integers(arr)
         expected = [1,2,3,0,5,4]
         self.assertTrue(output == expected)
 
-
-    def test_largest_sub_array_formed_by_consecutive_integers3(self):
         arr = []
         output = ar.largest_sub_array_formed_by_consecutive_integers(arr)
         expected = None
