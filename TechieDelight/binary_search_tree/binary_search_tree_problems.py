ddc9b48f281967b1a1b446bb494332475b6a2e82
paolo215 <paolo_villanueva215@yahoo.com>
2017-06-14 23:14:37 -0400

    Added comments

--- TechieDelight/binary_search_tree/binary_search_tree_problems.py
+++ TechieDelight/binary_search_tree/binary_search_tree_problems.py
@@ -1,9 +1,12 @@
 from Node import Node
 
 class BinarySearchTree:
+    # Binary Search Tree data structure. Assumes that the values of the Nodes
+    # are numbers.
     def __init__(self):
         self.root = None 
 
+    # Wrapper for inserting value to BST
     def insert(self, value):
         if self.root == None:
             self.root = Node(value)
@@ -11,12 +14,14 @@ class BinarySearchTree:
 
         self.__insert(self.root, value)
 
+    # Recursive insertion BST
     def __insert(self, root, value):
         if root == None:
             root = Node(value)
             print(root.getValue())
             return
 
+        # Go to the right if value >= value of the root
         if value >= root.getValue():
             if root.getRight():
                 insert(root.getRight(), value)
@@ -28,9 +33,12 @@ class BinarySearchTree:
             else:
                 root.setLeft(Node(value))
 
+    
+    # Wrapper for inorder traversal
     def inorder(self, data):
         self.__inorder(self.root, data)
 
+    # Inorder traversal
     def __inorder(self, root, data):
         if root == None:
             return
