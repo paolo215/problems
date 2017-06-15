223f2aac8ba0b27d47f2df17e913d0dbb2ee2345
paolo215 <paolo_villanueva215@yahoo.com>
2017-06-14 23:06:08 -0400

    Put all functions to a BST class

--- TechieDelight/binary_search_tree/binary_search_tree_problems.py
+++ TechieDelight/binary_search_tree/binary_search_tree_problems.py
@@ -1,27 +1,42 @@
 from Node import Node
 
-def insert(root, value):
-    if root == None:
-        root = Node(value)
-        print(root.getValue())
-        return
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None 
 
-    if value >= root.getValue():
-        if root.getRight():
-            insert(root.getRight(), value)
-        else:
-            root.setRight(Node(value))
-    else:
-        if root.getLeft():
-            insert(root.getLeft(), value)
-        else:
-            root.setLeft(Node(value))
+    def insert(self, value):
+        if self.root == None:
+            self.root = Node(value)
+            return
 
-def inorder(root, data):
-    if root == None:
-        return
-    inorder(root.getLeft(), data)
-    data.append(root.getValue())
-    inorder(root.getRight(), data)
+        self.__insert(self.root, value)
+
+    def __insert(self, root, value):
+        if root == None:
+            root = Node(value)
+            print(root.getValue())
+            return
+
+        if value >= root.getValue():
+            if root.getRight():
+                insert(root.getRight(), value)
+            else:
+                root.setRight(Node(value))
+        else:
+            if root.getLeft():
+                insert(root.getLeft(), value)
+            else:
+                root.setLeft(Node(value))
+
+    def inorder(self, data):
+        self.__inorder(self.root, data)
+
+    def __inorder(self, root, data):
+        if root == None:
+            return
+        self.__inorder(root.getLeft(), data)
+        data.append(root.getValue())
+        self.__inorder(root.getRight(), data)
     
     
+   
--- TechieDelight/binary_search_tree/test_binary_search_tree.py
+++ TechieDelight/binary_search_tree/test_binary_search_tree.py
@@ -5,11 +5,12 @@ from Node import Node
 
 class TestBinarySearchTree(unittest.TestCase):
     def test_insert(self):  
-        root = Node(4)
-        bst.insert(root, 2)
+        tree = bst.BinarySearchTree()
+        tree.insert(4)
+        tree.insert(2)
 
         inorder_output = []
-        bst.inorder(root, inorder_output)
+        tree.inorder(inorder_output)
         expected = [2, 4]
 
         self.assertTrue(expected == inorder_output)
